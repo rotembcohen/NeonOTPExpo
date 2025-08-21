2ba2b01892c092989a2e607a8186b852e1d662ef
Rotem Cohen <rotembcohen@gmail.com>
2025-08-21 11:47:03 -0400

    Add OTPScreen test for 6 input fields in App-test.tsx\n\n- Added OTPScreen import and test to App-test.tsx to verify 6 OTP input fields are rendered

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,11 +1,20 @@
 import { render } from '@testing-library/react-native';
 
 import App from '../App';
+import OTPScreen from '../OTPScreen';
 
 describe('<App />', () => {
   test('Text renders correctly on App', () => {
     const { getByText } = render(<App />);
 
     getByText('Welcome!');
+  });
+});
+
+describe('<OTPScreen />', () => {
+  test('renders 6 OTP input fields', () => {
+    const { getAllByTestId } = render(<OTPScreen />);
+    const inputs = getAllByTestId('otp-input');
+    expect(inputs).toHaveLength(6);
   });
 });
